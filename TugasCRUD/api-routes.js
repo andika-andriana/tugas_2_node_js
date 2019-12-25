12a144cf582bed9accb16d7b40267ecf27ec0012
Andika Andriana <andriana.mia1@gmail.com>
2019-12-26 01:46:08 +0700

    Tugas 2 Node JS
    Tugas ini Saya kumpulkan untuk Niomic.

---
+++ TugasCRUD/api-routes.js
@@ -0,0 +1,25 @@
+// fileName: api-routes.js
+
+// initialize Express router
+let router = require("express").Router();
+
+// set default API responses
+router.get("/", function(req, res) {
+  res.json({
+    status: "API its Working",
+    message: "Welcome to TugasCRUD crafted!"
+  });
+});
+
+// Import siswa controller
+var siswaController = require("./siswaController");
+
+// Contact routes
+router
+  .route("/siswa")
+  .get(siswaController.index)
+  .post(siswaController.new);
+router.route("/siswa/:siswa_id").get(siswaController.view);
+
+// exports API module
+module.exports = router;
